a6598f49e100a1e2775efe9f934b3b685041e329
liuxin <784532927@qq.com>
2018-02-26 17:25:29 +0800

    手写模板文件

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const Koa = require('koa');
 const app = new Koa();
+const { normal } = require('./views');
 
 app.use(async (ctx, next) => {
-    ctx.body = 'trailer';
+    ctx.type = 'text/html; charset=utf-8';
+    ctx.body = normal;
 })
 
 app.listen(8899);
