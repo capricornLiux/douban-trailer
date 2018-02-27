b094d78d8323dda29bdf47794805d18e866ae946
liuxin <784532927@qq.com>
2018-02-27 13:14:20 +0800

    pug模板结构拆分

--- server/index.js
+++ server/index.js
@@ -1,16 +1,23 @@
+const {resolve} = require('path');
 const Koa = require('koa');
 const app = new Koa();
 const pug = require('pug');
-const {htmlTpl, ejsTpl, pugTpl} = require('./views');
+// const {htmlTpl, ejsTpl, pugTpl} = require('./views');
 const ejs = require('ejs');
+const views = require('koa-views');
+
+app.use(views(resolve(__dirname,'./views'), {
+    extension: 'pug'
+}))
 
 app.use(async(ctx, next) => {
-    ctx.type = 'text/html; charset=utf-8';
-    // ctx.body = normal; ctx.body = ejs.render(ejsTpl, {     name: 'lance',
-    // address: 'China' })
-    ctx.body = pug.render(pugTpl, {
-        name: 'lucy',
-        address: 'Singapore'
+    // ctx.body = pug.render(pugTpl, {
+    //     name: 'lucy',
+    //     address: 'Singapore'
+    // })
+    await ctx.render('index',{
+        name: 'Lily',
+        address: 'Italy'
     })
 })
 
