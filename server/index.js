422e82c45baf077b2a87f963b269fd5391b282e6
liuxin <784532927@qq.com>
2018-02-27 11:23:37 +0800

    使用pug模板引擎

--- server/index.js
+++ server/index.js
@@ -1,14 +1,16 @@
 const Koa = require('koa');
 const app = new Koa();
-const { htmlTpl, ejsTpl } = require('./views');
+const pug = require('pug');
+const {htmlTpl, ejsTpl, pugTpl} = require('./views');
 const ejs = require('ejs');
 
-app.use(async (ctx, next) => {
+app.use(async(ctx, next) => {
     ctx.type = 'text/html; charset=utf-8';
-    // ctx.body = normal;
-    ctx.body = ejs.render(ejsTpl, {
-        name: 'lance',
-        address: 'China'
+    // ctx.body = normal; ctx.body = ejs.render(ejsTpl, {     name: 'lance',
+    // address: 'China' })
+    ctx.body = pug.render(pugTpl, {
+        name: 'lucy',
+        address: 'Singapore'
     })
 })
 
